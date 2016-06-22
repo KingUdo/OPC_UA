07e781831fd7a7a7dff7a3740d3391b092e43167
KingUdo <lars.koenig3@gmail.com>
2016-06-22 08:46:29 +0200

    Create Simple_Server.c

---
+++ Simple_Server.c
@@ -0,0 +1,32 @@
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+# include "ua_types.h"
+# include "ua_server.h"
+# include "ua_config_standard.h"
+# include "networklayer_tcp.h"
+
+UA_Boolean running = true;
+UA_Logger logger = Logger_Stdout;
+
+static void stopHandler(int sign) {
+    UA_LOG_INFO(logger, UA_LOGCATEGORY_SERVER, "received ctrl-c");
+    running = false;
+}
+
+//#######################################MAIN#######################################
+int main(int argc, char** argv) {
+    signal(SIGINT, stopHandler); /* catches ctrl-c */
+
+    UA_ServerConfig config = UA_ServerConfig_standard;
+    UA_ServerNetworkLayer nl = UA_ServerNetworkLayerTCP(UA_ConnectionConfig_standard, 16664);
+    config.networkLayers = &nl;
+    config.networkLayersSize = 1;
+    UA_Server *server = UA_Server_new(config);
+
+    UA_StatusCode retval = UA_Server_run(server, &running);
+    UA_Server_delete(server);
+    nl.deleteMembers(&nl);
+
+    return (int)retval;
+}
